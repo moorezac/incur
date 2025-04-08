5c35a068a51a2346b42f2a4f4ba46a22a110d075
moorezacmoore <moore.z@wehi.edu.au>
2025-04-08 17:47:33 +1000

    roxy

--- R/double_rate_funcs.R
+++ R/double_rate_funcs.R
@@ -1,5 +1,5 @@
 #' @title Calculate the double rate for area measurements of spheroids over time.
-#' @description From a set of matched values that represent area/growth readouts where one group is a known double of another, calculate a doubling time. This function was designed to measure spheroid area from IncuCyte images were cells were initially seeded at 1,000 and 2,000 cells separately. It is recommended that data be generated from the `fit_model` function, and then `predict_data` used to generate a large number of points to aid in calculations.
+#' @description From a set of matched values that represent area/growth readouts where one group is a known double of another, calculate a doubling time. This function was designed to measure spheroid area from IncuCyte images were cells were initially seeded at 1,000 and 2,000 cells separately. It is recommended that data be generated from the `fit_model` function with shared parameters, and then `predict_data` used to generate a large number of points to aid in calculations.
 #' @param data A `data.frame` or `data.frame` extension (tibble) in long format.
 #' @param x_var A character string that refers to the the `x` value within `data`.
 #' @param y_var A character string that refers to the the `y` value within `data`.
@@ -21,14 +21,14 @@ calc_double_rate_group <- function(data, x_var = "x", y_var = "y", shared_group 
   }
   
   # extract data
-  data_x <- filter(data, group == n) |> pull(x)
-  data_n <- filter(data, group == n) |> pull(y)
-  data_two_n <- filter(data, group == two_n) |> pull(y)
+  data_x <- dplyr::filter(data, group == n) |> dplyr::pull(x)
+  data_n <- dplyr::ilter(data, group == n) |> dplyr::pull(y)
+  data_two_n <- dplyr::filter(data, group == two_n) |> dplyr::pull(y)
   
   # ratio approach
   data_ratio <- log(data_two_n / data_n)
   
-  # assume sigmoid growth
+  # if assume sigmoid growth
   # when two_n > n the ratio max is around two_n inflection point
   max_index <- which(ratio == max(ratio))
   data_x_filt <- data_x[0:max_index]
@@ -49,34 +49,34 @@ calc_double_rate_group <- function(data, x_var = "x", y_var = "y", shared_group 
   double_rate <- log(2)/((2^(2/3)) * coef(linear_fit)[[2]])
   
   # plot
-  df_overall <- mutate(data, linear = case_when(x %in% data_x[linear_indexes] ~ TRUE, .default = FALSE))
-  df_overall <- mutate(df_overall, group = case_when(linear == TRUE ~ "linear", .default = group))
-  gg_overall <- ggplot(df_overall, mapping = aes(x, y, colour = group)) + 
-    geom_point() + 
-    scale_colour_manual(values = c("#FFC20A", "#0C7BDC", "#40B0A6") |> set_names(c(n, two_n, "linear")), breaks = c(n, two_n)) + 
-    geom_vline(xintercept = c(min(data_x_filt[linear_indexes]), max(data_x_filt[linear_indexes])), linetype = "dashed") + 
-    labs(x = x_lab, y = y_lab) + 
+  df_overall <- dplyr::mutate(data, linear = dplyr::case_when(x %in% data_x[linear_indexes] ~ TRUE, .default = FALSE))
+  df_overall <- dplyr::mutate(df_overall, group = dplyr::case_when(linear == TRUE ~ "linear", .default = group))
+  gg_overall <- ggplot2::ggplot(df_overall, mapping = aes(x, y, colour = group)) + 
+    ggplot2::geom_point() + 
+    ggplot2::scale_colour_manual(values = c("#FFC20A", "#0C7BDC", "#40B0A6") |> rlang::set_names(c(n, two_n, "linear")), breaks = c(n, two_n)) + 
+    ggplot2::geom_vline(xintercept = c(min(data_x_filt[linear_indexes]), max(data_x_filt[linear_indexes])), linetype = "dashed") + 
+    ggplot2::labs(x = x_lab, y = y_lab) + 
     theme_incur
   
   df_ratio <- data.frame(x = data_x, y = data_ratio)
-  df_ratio <- mutate(df_ratio, linear = case_when(x %in% data_x[linear_indexes] ~ TRUE, .default = FALSE))
-  gg_ratio <- ggplot(df_ratio, mapping = aes(x, y, colour = linear)) + 
-    geom_point() + 
-    scale_colour_manual(values = c("#40B0A6", "#E1BE6A") |> set_names(TRUE, FALSE)) + 
+  df_ratio <- dplyr::mutate(df_ratio, linear = dplyr::case_when(x %in% data_x[linear_indexes] ~ TRUE, .default = FALSE))
+  gg_ratio <- ggplot2::ggplot(df_ratio, mapping = aes(x, y, colour = linear)) + 
+    ggplot2::geom_point() + 
+    ggplot2::scale_colour_manual(values = c("#40B0A6", "#E1BE6A") |> rlang::set_names(TRUE, FALSE)) + 
     # geom_abline(intercept = coef(linear_fit)[[1]], slope = coef(linear_fit)[[2]]) +
     geomtextpath::geom_textabline(
       intercept = coef(linear_fit)[[1]], slope = coef(linear_fit)[[2]],
-      label = (str_c(
+      label = (stringr::str_c(
         signif(coef(linear_fit)[[1]], digits = 3), "\u00d7", "x + ", signif(coef(linear_fit)[[2]], digits = 3)
         )),
-      size = 6/.pt,
+      size = 6/ggplot2::.pt,
       vjust = 1.5
     ) +
-    labs(x = x_lab, y = str2expression(str_c("ln(", two_n, "/", n, ")")))  + 
+    ggplot2::labs(x = x_lab, y = str2expression(str_c("ln(", two_n, "/", n, ")")))  + 
     theme_incur
   
   gg <- patchwork::wrap_plots(gg_overall, gg_ratio, guides = "collect", axis_titles = "collect") + 
-    theme(aspect.ratio = 1)
+    ggplot2::theme(aspect.ratio = 1)
   
   return(list(double_rate = double_rate, double_plot = gg))
   
--- R/theme_incur.R
+++ R/theme_incur.R
@@ -4,29 +4,29 @@ font_small <- 8
 font_large <- 12
 linewidth <- 1
 
-theme_incur <- theme(
-  axis.line = element_line(linewidth = linewidth / .pt),
-  axis.text = element_text(size = font_small),
-  axis.title.x = element_text(size = font_size),
-  axis.title.y = element_text(size = font_size),
+theme_incur <- ggplot2::theme(
+  axis.line = ggplot2::element_line(linewidth = linewidth / ggplot2::.pt),
+  axis.text = ggplot2::element_text(size = font_small),
+  axis.title.x = ggplot2::element_text(size = font_size),
+  axis.title.y = ggplot2::element_text(size = font_size),
   # legend.box = "horizontal",
   # legend.location = "plot",
   # legend.justification = c(1, 1),
-  legend.box.margin = margin(0, 0, 0, 0),
-  legend.key = element_rect(colour = "black", linewidth = linewidth / .pt),
-  legend.key.size = unit(1.25 * font_size, "pt"),
-  legend.title = element_text(size = font_size),
-  panel.grid = element_line(
+  legend.box.margin = ggplot2::margin(0, 0, 0, 0),
+  legend.key = ggplot2::element_rect(colour = "black", linewidth = linewidth / ggplot2::.pt),
+  legend.key.size = ggplot2::unit(1.25 * font_size, "pt"),
+  legend.title = ggplot2::element_text(size = font_size),
+  panel.grid = ggplot2::element_line(
     colour = "grey90",
-    linewidth = linewidth / .pt
+    linewidth = linewidth / ggplot2::.pt
   ),
-  panel.background = element_rect(fill = "white"),
+  panel.background = ggplot2::element_rect(fill = "white"),
   # panel.border = element_rect(
   #   colour = "black",
   #   fill = NA,
   #   linewidth = linewidth / .pt
   # ),
-  panel.border = element_blank(),
-  plot.title = element_text(size = font_size),
-  text = element_text(size = font_size)
+  panel.border = ggplot2::element_blank(),
+  plot.title = ggplot2::element_text(size = font_size),
+  text = ggplot2::element_text(size = font_size)
 )
